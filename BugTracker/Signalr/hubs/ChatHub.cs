32e76cbe0442a43f953e702a4aee39d53107f159
swami1234 <34219565+swami1234@users.noreply.github.com>
2019-10-02 11:33:52 -0400

    Few more changes

--- BugTracker/Signalr/hubs/ChatHub.cs
+++ BugTracker/Signalr/hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -8,10 +9,11 @@ namespace BugTracker.Signalr.hubs
 {
     public class ChatHub : Hub
     {
+        
 
         public void send(string name, string message)
         {
-            Clients.All.addNewMessageToPage(name, message);
+            Clients.All.addNewMessageToPage(name, message, DateTime.Now);
         }
 
        
